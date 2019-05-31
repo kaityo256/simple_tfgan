cd157fce9f03398c86eb32c8b3461a403e2048a1
H. Watanabe <kaityo@users.sourceforge.jp>
2019-05-31 15:45:35 +0900

    adds fontawesome.py

--- fontawesome.py
+++ fontawesome.py
@@ -35,26 +35,27 @@ def image_to_tfexample(image_data, image_format):
     }))
 
 
-font = "./ipaexg.ttf"
+font = "./fa-solid-900.ttf"
 
 if not os.path.exists(font):
     print("Could not find a font file %s." % font)
     print("Please down load from")
-    print("https://ipafont.ipa.go.jp/old/ipafont/download.html")
+    print("https://fontawesome.com/")
     exit()
+
+icons = [0xf0fc, 0xf0f3, 0xf02d, 0xf518, 0xf207,
+         0xf1b9, 0xf5e4, 0xf1ae, 0xf64f, 0xf51e]
 
 num_images = 10000
 images = np.empty((num_images, 28, 28, 1), dtype=np.uint8)
 for i in range(num_images):
     img = Image.new('L', (28, 28))
     draw = ImageDraw.Draw(img)
-    draw.font = ImageFont.truetype(font, 28)
-    x = random.randint(-2, 2)
-    y = random.randint(-2, 2)
-    theta = random.randint(-10, 10)
-    kana = chr(random.randint(ord('あ'), ord('ん')))
-    draw.text((x, y), kana, (255))
-    img = img.rotate(theta)
+    draw.font = ImageFont.truetype(font, 24)
+    x = random.randint(2, 4)
+    y = random.randint(2, 4)
+    text = chr(random.choice(icons))
+    draw.text((x, y), text, (255))
     nim = np.array(img)
     nim = nim.reshape((28, 28, 1))
     images[i:] = nim
@@ -63,15 +64,14 @@ for i in range(num_images):
     #filename = "test%04d.png" % i
     # img.save(filename)
 
-print()
 
 image = tf.placeholder(dtype=tf.uint8, shape=(28, 28, 1))
 encoded_png = tf.image.encode_png(image)
 with tf.Session('') as sess:
-    with tf.python_io.TFRecordWriter("hiragana.tfrecord") as tf_writer:
+    with tf.python_io.TFRecordWriter("fontawesome.tfrecord") as tf_writer:
         for i in range(num_images):
             png_string = sess.run(encoded_png, feed_dict={image: images[i]})
             example = image_to_tfexample(png_string, 'png'.encode())
             tf_writer.write(example.SerializeToString())
 
-print('Successfully generated hiragana.tfrecord.')
+print('Successfully generated fontawesome.tfrecord.')
